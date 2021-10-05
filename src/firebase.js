1aed827a9607de1ec44787267dcf2bc72a987579
CHAMYJ <43520507+CHAMYJ@users.noreply.github.com>
2021-10-05 18:10:33 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAWMF1dpYnA3bZRl5y2btgwSygMWyu-BY0",
-  authDomain: "weather-dashboard-105b6.firebaseapp.com",
-  projectId: "weather-dashboard-105b6",
-  storageBucket: "weather-dashboard-105b6.appspot.com",
-  messagingSenderId: "549717037516",
-  appId: "1:549717037516:web:ffa649ccd4ff801a680d69",
-  measurementId: "G-3RBB6081ZJ",
+  // Please put you own config from firebase
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
